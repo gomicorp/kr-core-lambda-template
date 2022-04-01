02625de1821b52f2bd2621b29ba95eb75a4e97db
uiseok.choi <gobeck84@gmail.com>
2022-04-01 17:25:44 +0900

    ♿️ graphql 호출 예시를 추가 했습니다.

--- node-crawler-to-dynamoDB-typescript/src/query.ts
+++ node-crawler-to-dynamoDB-typescript/src/query.ts
@@ -36,7 +36,7 @@ export const sampleQuery = JSON.stringify({ query: getBoardQuery });
 
 // variable( 파라미터 ) 받을 수 있게 하는 예시 입니다.
 // gql 사용하지 않고 query 부분부터 사용하셔도 됩니다.
-export const sampleQueryWithGql = {
+const sampleQueryWithGql = {
   sampleQueryWithGql: gql`
     query getBoardQuery($boardId: String) {
         boards(ids: $boardId) {
@@ -68,4 +68,7 @@ export const sampleQueryWithGql = {
         }
     }
   `
+}
+export const sampleQueryFunction = ( boardId: string ) => {
+    sampleQueryWithGql.sampleQueryWithGql({ variables: { boardId } })
 }
